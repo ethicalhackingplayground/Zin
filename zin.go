8f79ef2814d552224cacf2ae87d8d7ba94b5ba5e
Krypt0mux <33673319+ethicalhackingplayground@users.noreply.github.com>
2020-06-14 22:56:39 +1000

    Add files via upload

--- zin.go
+++ zin.go
@@ -52,6 +52,7 @@ func main() {
 	// Payload to be used.
 	concurrPtr:= flag.Int("c", 20, "the concurrency")
 	payloadPtr := flag.String("p", "", "the payload to be used")
+	payloadsPtr := flag.String("pL", "", "the list of payloads to be used")
 
 	// Parse the arguments
 	flag.Parse()
@@ -61,19 +62,43 @@ func main() {
 		flag.PrintDefaults()
 		return	
 	}else{
-		// Implement Concurrency
-        	var wg sync.WaitGroup
-        	for i := 0; i < *concurrPtr/2; i++ {
-                	wg.Add(1)
-                	go func() {
-                        	// Run the scanner
-                        	run(*payloadPtr)
-                        	wg.Done()
-               		}()
-                	wg.Wait()
-        	}
-	}
+        
 
+		// Create the output directory
+ 		_,err := os.Stat("output")
+ 		if os.IsNotExist(err) {
+   			errDir := os.Mkdir("output", 0755)
+   			if errDir != nil {
+      				log.Fatal(err)
+   			}
+		}
+		if *payloadsPtr != "" && *payloadPtr == "" {
+			 // Implement Concurrency
+                	var wg sync.WaitGroup
+               	 	for i := 0; i < *concurrPtr/2; i++ {
+                        	wg.Add(1)
+                        	go func() {
+                                	// Run the scanner
+                                	runWithMultiplePayload(*payloadsPtr)
+                                	wg.Done()
+                        	}()
+                        	wg.Wait()
+                	}
+		}else{
+
+			// Implement Concurrency
+        		var wg sync.WaitGroup
+        		for i := 0; i < *concurrPtr/2; i++ {
+               	 		wg.Add(1)
+                		go func() {
+                        		// Run the scanner
+                        		runWithSinglePayload(*payloadPtr)
+                        		wg.Done()
+               			}()
+                		wg.Wait()
+        		}
+	        }
+        }
 }
 
 // Print the banner
@@ -97,8 +122,88 @@ May the bounties come
 	fmt.Println(Red + m1 + Cyan + m2) 
 }
 
+
 // Read the file containing the urls from stdin
-func run(payload string) {
+func runWithMultiplePayload(payloads string) {
+
+ fmt.Println(White + "[" + Blue + "~" + White + "] Searching for URL(s)")
+ fmt.Println(White + "[" + Green+ "~" + White + "]" + Red + " Multiple Payloads")
+ fmt.Println(White + "========================================================================================\n")
+
+ fmt.Println("Status Code\tBytes\t\tURL")
+ fmt.Println("-----------\t-----\t\t---\n")
+
+ 	
+ // Create the 'NewScanner' object and print each line in the file
+ scanner := bufio.NewScanner(os.Stdin)
+ for scanner.Scan() {
+
+
+ 	// Parse the URL
+  	u,err := url.Parse(scanner.Text())
+   	if err != nil{
+      		log.Fatal(err)
+   	}
+   	// Fetch the URL Values
+	qs := url.Values{}
+
+	file,err := os.Open(payloads)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	pL := bufio.NewScanner(file)
+	for pL.Scan() {
+
+
+	   	// Get the url paraemters and set the newvalue (payload)
+   		for param,vv := range u.Query() {
+      			qs.Set(param, vv[0]+pL.Text())
+  		}
+	}
+	if err := pL.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	// Url encoding the url
+	u.RawQuery = qs.Encode()
+
+	// Create the response file
+        f,err := os.Create("output/responses.txt")
+        if err != nil {
+        	log.Fatal(err)
+        }
+
+        // Dump the response
+        resp,err := http.Get(scanner.Text())
+   	if err != nil {
+      		log.Fatal(err)
+       	}
+
+   	dump,err := httputil.DumpResponse(resp, true)
+   	if err != nil {
+      		log.Fatal(err)
+   	}
+  	l,err := f.WriteString(string(dump))
+   	if err != nil {
+        	log.Fatal(err)
+        }
+
+	defer resp.Body.Close()	
+   	// Print the values
+   	fmt.Printf("%s\t", resp.StatusCode)
+   	fmt.Printf("%d Bytes\t", l)
+   	fmt.Println(White + "[" + Green + "~" + White + "] " + White + u.String())
+ }
+ if err := scanner.Err(); err != nil {
+   log.Fatal(err)
+ }
+}
+
+
+// Read the file containing the urls from stdin
+func runWithSinglePayload(payload string) {
 
 	fmt.Println(White + "[" + Blue + "~" + White + "] Searching for URL(s)")
 	fmt.Println(White + "[" + Green+ "~" + White + "] Payload: " + payload)
@@ -106,20 +211,6 @@ func run(payload string) {
 		
 	fmt.Println("Status Code\tBytes\t\tURL")
 	fmt.Println("-----------\t-----\t\t---\n")
-	// Create the output directory
-	_,err := os.Stat("output")
-	if os.IsNotExist(err) {
-		errDir := os.Mkdir("output", 0755)	
-		if errDir != nil {
-			log.Fatal(err)
-		}
-	}
-
-	// Create the response file
-	f,err := os.Create("output/responses.txt")
-        if err != nil{
-        	log.Fatal(err)
-        }
 
 	// Create the 'NewScanner' object and print each line in the file
 	scanner := bufio.NewScanner(os.Stdin)
@@ -141,11 +232,17 @@ func run(payload string) {
 		}
 
 		// Url encoding the url
-		//u.RawQuery = qs.Encode()
+		u.RawQuery = qs.Encode()
 		
 		// Dump the response
 		resp,err := http.Get(scanner.Text())
 		if err != nil {
+                        log.Fatal(err)
+                }
+
+		// Create the response file
+                f,err := os.Create("output/responses.txt")
+                if err != nil {
                         log.Fatal(err)
                 }
 
