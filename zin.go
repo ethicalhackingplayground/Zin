4f0407df09a32095ec7f52190740ef971d8e837f
Krypt0mux <33673319+ethicalhackingplayground@users.noreply.github.com>
2020-06-14 18:16:38 +1000

    Add files via upload

---
+++ zin.go
@@ -0,0 +1,160 @@
+//
+// Payload Injector
+//
+// Coder: zoid
+
+package main
+
+import (
+"flag"
+"log"
+"bufio"
+"os"
+"fmt"
+"time"
+"net/url"
+"net/http"
+"net/http/httputil"
+"runtime"
+)
+
+
+var Reset   = "\033[0m"
+var Red     = "\033[31m"
+var Green   = "\033[32m"
+var Yellow  = "\033[33m"
+var Blue    = "\033[34m"
+var Purple  = "\033[35m"
+var Cyan    = "\033[36m"
+var Gray    = "\033[37m"
+var White   = "\033[97m"
+
+func init() {
+	if runtime.GOOS == "windows" {
+		Reset   = ""
+		Red     = ""
+		Green   = ""
+		Yellow  = ""
+		Blue    = ""
+		Purple  = ""
+		Cyan    = ""
+		Gray    = ""
+		White   = ""
+	}
+}
+
+
+func main() {
+	
+	// Payload to be used.
+	payloadPtr := flag.String("p", "", "the payload to be used")
+
+	// Parse the arguments
+	flag.Parse()
+
+	// Print the help 
+	if *payloadPtr == "" {
+		flag.PrintDefaults()
+		return	
+	}
+
+	// Banner
+	banner()
+
+	// Run the scanner
+	run(*payloadPtr)
+}
+
+// Print the banner
+func banner() {
+	
+
+	m1 := `	
+      _       
+     (_)      
+  _____ _ __  
+ |_  / | '_ \ 
+  / /| | | | |
+ /___|_|_| |_|
+
+	`
+	m2 := `
+May the bounties come
+
+	`
+	
+	fmt.Println(Red + m1 + Cyan + m2)
+	time.Sleep(2 * time.Second)
+}
+
+// Read the file containing the urls from stdin
+func run(payload string) {
+
+	fmt.Println(White + "[" + Blue + "~" + White + "] Searching for URL(s)")
+	fmt.Println(White + "[" + Green+ "~" + White + "] Payload: " + payload)
+	fmt.Println(White + "========================================================================================\n")
+		
+	fmt.Println("Status Code\tBytes\t\tURL")
+	fmt.Println("-----------\t-----\t\t---\n")
+	// Create the output directory
+	_,err := os.Stat("output")
+	if os.IsNotExist(err) {
+		errDir := os.Mkdir("output", 0755)	
+		if errDir != nil {
+			log.Fatal(err)
+		}
+	}
+
+	// Create the response file
+	f,err := os.Create("output/responses.txt")
+        if err != nil{
+        	log.Fatal(err)
+        }
+
+	// Create the 'NewScanner' object and print each line in the file
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+
+
+		// Parse the URL
+		u,err := url.Parse(scanner.Text())
+		if err != nil{
+			log.Fatal(err)
+		}
+		// Fetch the URL Values
+		qs := url.Values{}
+		
+
+		// Get the url paraemters and set the newvalue (payload)
+		for param,vv := range u.Query() {
+			qs.Set(param, vv[0]+payload)
+		}
+
+		// Url encoding the url
+		u.RawQuery = qs.Encode()
+		
+		// Dump the response
+		resp,err := http.Get(scanner.Text())
+		defer resp.Body.Close()
+		if err != nil{
+                        log.Fatal(err)
+                }
+
+		dump,err := httputil.DumpResponse(resp, true)
+		if err != nil{
+			log.Fatal(err)
+		}
+		l,err := f.WriteString(string(dump))
+		if err != nil{
+                        log.Fatal(err)
+                }
+
+		// Print the values
+		fmt.Printf("%s\t", resp.StatusCode)
+		fmt.Printf("%d Bytes\t", l)
+		fmt.Println(White + "[" + Green + "~" + White + "] " + White + u.String()) 
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+}
